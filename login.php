aac1f95542b58ab1faa464691166a85cd9742b0a
Karl Jasen <karljasentapangan@gmail.com>
2020-02-24 10:00:41 +0800

    First

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+    <script src="bootstrap.js"></script>
+    <script src="bootstrap.min.js"></script>
+</body>
+</html>
